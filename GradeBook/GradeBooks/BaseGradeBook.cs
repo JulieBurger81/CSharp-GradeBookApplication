95e3cc857bfb9bda7c22771674f3a73f2aa02bb6
JulieBurger81 <61249866+JulieBurger81@users.noreply.github.com>
2020-04-11 14:44:29 -0500

    C#-7 - Adding all Weighted support items

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,13 @@ namespace GradeBook.GradeBooks
         public GradeBookType Type { get; set; }
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool IsWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            bool IsWeighted;
         }
 
         public void AddStudent(Student student)
@@ -110,15 +112,40 @@ namespace GradeBook.GradeBooks
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    if (IsWeighted == true) 
+                        return 5;
+                    else
+                    {
+                        return 4;
+                    }
                 case 'B':
-                    return 3;
+                    if (IsWeighted == true)
+                        return 4;
+                    else
+                    {
+                        return 3;
+                    }
                 case 'C':
-                    return 2;
+                    if (IsWeighted == true)
+                        return 3;
+                    else
+                    {
+                        return 2;
+                    };
                 case 'D':
-                    return 1;
+                    if (IsWeighted == true)
+                        return 2;
+                    else
+                    {
+                        return 1;
+                    };
                 case 'F':
-                    return 0;
+                    if (IsWeighted == true)
+                        return 1;
+                    else
+                    {
+                        return 0;
+                    }
             }
             return 0;
         }
