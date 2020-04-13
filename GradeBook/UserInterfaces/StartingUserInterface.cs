e6b5bfe91e2b3d52c300e8c92f4203ed4ff64736
JulieBurger81 <61249866+JulieBurger81@users.noreply.github.com>
2020-04-13 15:19:30 -0500

    C#-7 reomving IsWeighted to get video?

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,13 +41,13 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2];
-            bool isWeighted = parts[3];
+            var isWeighted = parts[3];
 
             BaseGradeBook gradeBook; 
             if (type == "standard")
-                gradeBook = new StandardGradeBook(name, IsWeighted);
+                gradeBook = new StandardGradeBook(name, isWeighted);
             if (type == "ranked")
-                gradeBook = new RankedGradeBook(name, IsWeighted);
+                gradeBook = new RankedGradeBook(name, isWeighted);
             else
             {
                 Console.WriteLine("{0} is not a supported type of gradebook, please try again",type);
